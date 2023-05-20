ce81860e8755640596a28f1a0d4e397bc17fd68c
kyamisuke <murakami.k.am@m.titech.ac.jp>
2023-05-20 16:39:40 +0900

    Initial Commit

---
+++ StudyTime/StudyTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyTimeApp.swift
+//  StudyTime
+//
+//  Created by 村上航輔 on 2023/05/20.
+//
+
+import SwiftUI
+
+@main
+struct StudyTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
